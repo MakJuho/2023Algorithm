62c399c27a77655eace49e286a9614472cd3aeb2
우주호 <einjuho7@ajou.ac.kr>
2020-08-05 22:52:50 +0900

    BOJ_19236_청소년상어#1

---
+++ src/삼성SW코딩테스트/BOJ_19236_청소년상어.java
@@ -0,0 +1,24 @@
+package 삼성SW코딩테스트;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class BOJ_19236_청소년상어 {
+
+	
+	
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new StringReader(src));
+		
+		
+		
+	}
+	
+	
+	static String src = "7 6 2 3 15 6 9 8\r\n" + 
+			"3 1 1 8 14 7 10 1\r\n" + 
+			"6 1 13 6 4 3 11 4\r\n" + 
+			"16 1 8 7 5 2 12 2";
+}
