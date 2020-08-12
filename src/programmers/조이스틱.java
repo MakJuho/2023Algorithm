60363025c9765fcb2bf6e293a5643379dc9f1fc6
우주호 <einjuho7@ajou.ac.kr>
2020-08-13 01:34:38 +0900

    프로그래머스 조이스틱

--- src/programmers/조이스틱.java
+++ src/programmers/조이스틱.java
@@ -5,25 +5,40 @@ import java.util.Arrays;
 public class 조이스틱 {
 	
 	public static void main(String[] args) {
-		String name = "JEROEN";
+		String name = "BBAA";
 		
 		char[] arr = new char[name.length()];
-		char[] cmp = new char[name.length()];
 		
-		
-		for(int i=0; i<name.length(); i++) {
-			arr[i] = name.charAt(i);
-		}
-
-		Arrays.fill(cmp, 'A');
-		
-		
+		int cntUpDown = 0;
 		
 		// 숫자 A~Z 까지 있을 때, 위로 올라가는 게 빠른지 밑으로 내려가는 게 빠른지
+		for(int i=0; i<name.length(); i++) {
+			arr[i] = name.charAt(i);
+			int diff = (int)arr[i]-'A';
+			int val =0;
+			if(diff > 13) {
+				cntUpDown += 26-diff;
+			}else {
+				cntUpDown += diff;
+			}
+		}
+		
 		// 위치 오른쪽으로 가는 게 빠른지, 왼쪽으로 가는게 빠른지 체크
 		
+		int minMove = name.length() - 1;
+        for(int i = 0 ; i < name.length() ; i++) {
+            if(name.charAt(i) != 'A') {
+                int next = i+1;
+                while(next < name.length() && name.charAt(next) == 'A') {
+                    next++;
+                }
+                int move = 2 * i + name.length() - next;
+                minMove = Math.min(move, minMove);
+            }
+        }
 		
 		
+		System.out.println(minMove + cntUpDown);
 		
 		
 		
